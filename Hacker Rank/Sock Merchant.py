fd7403f28135f94a19f03f3a8ee044336f68dac9
chrispun0518 <62996645+chrispun0518@users.noreply.github.com>
2020-04-07 19:19:12 +0800

    Create Sock Merchant.py

---
+++ Hacker Rank/Sock Merchant.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    pairs = 0
+    ar = sorted(ar)
+    i = 0
+    while i < len(ar):
+        print(i)
+        if i + 1 != len(ar) and ar[i] == ar[i+1]:
+            i +=  1
+            pairs += 1
+        i += 1
+    return pairs
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

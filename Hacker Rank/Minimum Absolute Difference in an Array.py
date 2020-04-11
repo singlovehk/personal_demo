dee1ef6d704776a7ab2737bab291d5c780e9106e
chrispun0518 <62996645+chrispun0518@users.noreply.github.com>
2020-04-11 12:10:29 +0800

    Update Minimum Absolute Difference in an Array.py

--- Hacker Rank/Minimum Absolute Difference in an Array.py
+++ Hacker Rank/Minimum Absolute Difference in an Array.py
@@ -8,11 +8,12 @@ import sys
 
 # Complete the minimumAbsoluteDifference function below.
 def minimumAbsoluteDifference(arr):
-    mad = []
-    for index, v1 in enumerate(arr):
-        for v2 in arr[index+1:]:
-            mad.append(abs(v1 - v2))
-    return min(mad)
+    arr = sorted(arr, key=abs)
+    ans = []
+    for i in range(len(arr) - 1):
+        ans.append(abs(arr[i] - arr[i+1]))
+    return min(ans)
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
